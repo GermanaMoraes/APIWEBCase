2274210daf90220441c2e13155ed494cdcfc2b9d
Germana Moraes <germanamoraes95@gmail.com>
2022-09-29 13:56:10 -0300

    Create ConsultumTest

---
+++ TestProject/ConsultumTest.cs
@@ -0,0 +1,29 @@
+﻿using DesafioCase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CaseTests
+{
+    public class ConsultumTests
+    {
+        [Fact]
+        public void RetornarConsultumNotNull()
+        {
+            //preparação
+            Consultum consultum;
+
+            //Execução
+            consultum = new Consultum();
+
+            //Retorno Esperado
+            Assert.NotNull(consultum);
+
+
+
+        }
+    }
+}
