02d19085d62a6cd5ef963105e0bdabeda115c3b8
Germana Moraes <germanamoraes95@gmail.com>
2022-09-29 14:01:02 -0300

    Create EspecialidadeTest

---
+++ TestProject/EspecialidadeTest.cs
@@ -0,0 +1,29 @@
+﻿using DesafioCase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CaseTests
+{
+    public class EspecialidadeTest
+    {
+        [Fact]
+        public void RetornarEspecialidadNotNull()
+        {
+            //preparação
+            Especialidade especialidade;
+
+            //Execução
+            especialidade = new Especialidade();
+
+            //Retorno Esperado
+            Assert.NotNull(especialidade);
+
+
+
+        }
+    }
+}
